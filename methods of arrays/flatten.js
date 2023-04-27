3da9ca165f25a947e3cffe91cc59f223294c3e91
ascent2009 <ascent2009@hotmail.com>
2023-04-28 00:10:36 +0300

    arrays flatten

--- methods of arrays/flatten.js
+++ methods of arrays/flatten.js
@@ -5,5 +5,8 @@
     Например, flatten([1, [2, 3], [4, [5, 6]]]) должна вернуть [1, 2, 3, 4, 5, 6].
 * */
 function flatten(arr) {
-    // Your code
+    return arr
+        .join()
+        .split(",")
+        .map((a) => parseInt(a));
 }
