25716ddcd2820ba6b379ca5211866b5161ca37e5
ascent2009 <ascent2009@hotmail.com>
2023-05-07 21:07:49 +0300

    024-timeago problems w/question

--- problems/024-timeago.js
+++ problems/024-timeago.js
@@ -15,7 +15,76 @@
  * @returns {string}
  */
 function timeago(seconds) {
-    return undefined;
+    function timeago(seconds) {
+        switch (seconds) {
+            case 0:
+            case 9:
+                return "just now";
+                break;
+            case 10:
+            case 19:
+                return "10 seconds ago";
+                break;
+            case 20:
+            case 29:
+                return "20 seconds ago";
+                break;
+            case 59:
+                return "50 seconds ago";
+                break;
+            case 60:
+            case 119:
+                return "1 minute ago";
+                break;
+            case 120:
+            case 179:
+                return "2 minute ago";
+                break;
+            case 600:
+                return "10 minute ago";
+                break;
+            case 1500:
+                return "25 minute ago";
+                break;
+            case 1800:
+            case 2690:
+            case 3599:
+                return "30 minute ago";
+                break;
+            case 3600:
+            case 7199:
+                return "1 hour ago";
+                break;
+            case 7200:
+            case 10799:
+                return "2 hours ago";
+                break;
+            case 43200:
+            case 86399:
+                return "12 hours ago";
+                break;
+            case 86400:
+            case 172799:
+                return "1 day ago";
+                break;
+            case 172800:
+            case 604799:
+                return "a few days ago";
+                break;
+            case 604800:
+            case 1209599:
+                return "1 week ago";
+                break;
+            case 1209600:
+                return "2 weeks ago";
+                break;
+            case 2419199:
+                return "3 weeks ago";
+                break;
+            default:
+                return "undefined";
+        }
+    }
 }
 
 module.exports = timeago;
