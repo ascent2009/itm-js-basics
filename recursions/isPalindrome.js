42ae0d399a78271a907d6820315b8dd6b6107a66
ascent2009 <ascent2009@hotmail.com>
2023-05-11 17:44:37 +0300

    isPalindrome unsolved

--- recursions/isPalindrome.js
+++ recursions/isPalindrome.js
@@ -5,6 +5,27 @@
  */
 function isPalindrome(str) {
     // ваш код здесь
+    let string = str
+        .replaceAll(" ", "")
+        .replaceAll(",", "")
+        .replaceAll(":", "")
+        .toLowerCase();
+
+    if (string.length <= 1) {
+        return true;
+    }
+    let firstLetter = string[0];
+    let lastLetter = string[string.length - 1];
+
+    if (firstLetter === lastLetter) {
+        let stringWithoutFirstAndLastLetters = string.substring(
+            1,
+            string.length - 1
+        );
+        return isPalindrome(stringWithoutFirstAndLastLetters);
+    } else {
+        return false;
+    }
 }
 
 // Примеры:
