a6fb2e6124b4efd90cb8ab324112ed6964f5f6e0
ascent2009 <ascent2009@hotmail.com>
2023-05-15 14:20:26 +0300

    shuffle changed function params

--- methods of arrays/shuffle.js
+++ methods of arrays/shuffle.js
@@ -3,7 +3,7 @@
  порядке. Функция должна изменять исходный массив, а не возвращать новый. Например, shuffle([1, 2, 3, 4, 5]) может
  вернуть [3, 1, 5, 4, 2].
  * */
-function shuffle(arr1, arr2) {
+function shuffle(arr) {
     // Your code
     return arr
         .slice(0, 1)
